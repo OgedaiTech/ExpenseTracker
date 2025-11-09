9351106577bb71bfbc11c95a49d7e431e363afc1
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2025-11-09 21:37:05 +0000

    Refactor PostgreSQL configuration in Program.cs and update image tag; remove unused project reference in ExpenseTracker.Tenants.csproj

--- src/ExpenseTracker.AppHost/Program.cs
+++ src/ExpenseTracker.AppHost/Program.cs
@@ -6,9 +6,12 @@ var userName = builder.AddParameter("Username", "postgres");
 var password = builder.AddParameter("password", "h-HhSCtzy{0qd}kHnwZNYk");
 
 var postgresDbServer = builder
-  .AddPostgres(name: "postgres", userName: userName, password: password, port: 53096)
+  .AddPostgres(name: "postgres")
+  .WithUserName(userName)
+  .WithPassword(password)
+  .WithHostPort(53096)
   .WithDataVolume()
-  .WithPgAdmin(configureContainer: p => p.WithImageTag("9.8"));
+  .WithPgAdmin(configureContainer: p => p.WithImageTag("9.9"));
 var postgresDb = postgresDbServer.AddDatabase("ExT");
 
 var migrations = builder.AddProject<ExpenseTracker_MigrationService>("migration")
