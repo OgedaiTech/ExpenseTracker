50286e834706d372e3b2d4667191551c8d567377
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2026-01-06 12:29:53 +0300

    removed redundant class

--- src/UI/ExpenseTrackerUI/Services/ITokenStorageService.cs
+++
@@ -1,9 +0,0 @@
-namespace ExpenseTrackerUI.Services;
-
-public interface ITokenStorageService
-{
-  Task StoreTokensAsync(string accessToken, string refreshToken);
-  Task<string?> GetAccessTokenAsync();
-  Task<string?> GetRefreshTokenAsync();
-  Task ClearTokensAsync();
-}
